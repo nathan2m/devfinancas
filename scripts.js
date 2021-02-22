2007831bafd5260da072753556c6b8e5a3797b22
nathan2m <nathan.manera@gmail.com>
2021-02-22 11:00:34 -0300

    Bug correction

--- scripts.js
+++ scripts.js
@@ -106,9 +106,9 @@ const DOM = {
 
 const Utils = {
     formatAmount(value) {
-        value = Number(value.replace(/\,\./g, "")) * 100;
+        value = Number(value) * 100;
 
-        return value;
+        return Math.round(value);
     },
 
     formatDate(date) {
